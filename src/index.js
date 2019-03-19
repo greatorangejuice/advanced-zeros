96b346f6642714b1b4e52fe4b8a24f72888419b3
greatorangejuice <bypavelsnigirev@gmail.com>
2019-03-19 10:24:30 +0300

    done(but need rewrite)

--- src/index.js
+++ src/index.js
@@ -51,32 +51,54 @@ module.exports = function getZerosCount(number, base) {
 
   let result = [];
   ////////////
-                          // for (let i = 0; i < simpleArray.length; i++) {
-                          //   result.push(getSum(simpleArray[i]));
-                          // }
+                          for (let i = 0; i < simpleArray.length; i++) {
+                            result.push(getSum(simpleArray[i]));
+                          }
+                          console.log(`Суммы чисел после деления: `, result)
   ///////////
   // FIND MIN
-  let maxSimpleNumber = Math.max.apply(null, simpleArray);
-  console.log(`TEST: ${maxSimpleNumber}`);  
-  result.push(getSum(maxSimpleNumber));
 
-  console.log(`Массив суммы делений(result) = ${result}`);
-    
+  // if (simpleBase.length != simpleArray.length && simpleArray.length == 1) {
+  //   return (result[0] / simpleBase.length)^0
+  // } else {
+  //   return Math.min.apply(null, result)
+  // }
   const getDivider = (searchNumb) => {
     let counter = simpleBase.filter((item) => {
       return item == searchNumb;
     })
     return counter.length;
   }
-  console.log("COUNTER: " + getDivider(maxSimpleNumber));
-  let devider = getDivider(maxSimpleNumber);
-  // if (simpleBase.length != simpleArray.length && simpleArray.length == 1) {
-  //   return (result[0] / simpleBase.length)^0
-  // } else {
-  //   return Math.min.apply(null, result)
-  // }
 
-  return (result[0]/devider)^0;
+  let uniqueNumbersCounter = [];
+  for (let i = 0; i < simpleArray.length; i++) {
+    uniqueNumbersCounter.push(getDivider(simpleArray[i]));
+  }
+  console.log('КОЛИЧЕСТВО ЧИСЕЛ(ДЕЛИТЕЛЬ): ', uniqueNumbersCounter)
+
+  let minResult = [];
+  for (let i = 0; i < result.length; i++) {
+    minResult.push( result[i] / uniqueNumbersCounter[i] );
+  }
+  console.log('ОТВЕТ: ', Math.min.apply(null, minResult)^0);
+  return Math.min.apply(null, minResult)^0;
+  /* Через одно минимальное число */
+  // let maxSimpleNumber = Math.max.apply(null, simpleArray);
+  // console.log(`TEST: ${maxSimpleNumber}`);  
+  // result.push(getSum(maxSimpleNumber));
+
+  // console.log(`Массив суммы делений(result) = ${result}`);
+    
+  // const getDivider = (searchNumb) => {
+  //   let counter = simpleBase.filter((item) => {
+  //     return item == searchNumb;
+  //   })
+  //   return counter.length;
+  // }
+  // console.log("COUNTER: " + getDivider(maxSimpleNumber));
+  // let devider = getDivider(maxSimpleNumber);
+  // return (result[0]/devider);
+  /* Через одно минимальное число */
 
 }
 
